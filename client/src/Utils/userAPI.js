0ea78af7c0bcbdbf9d83c9c5debdcaf6549f677f
viktor vaughn <dereck8187@gmail.com>
2021-02-04 21:35:25 -0500

    user and bike equipment

---
+++ client/src/Utils/userAPI.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+export const API = {
+    getAllUsers: () => {
+        return axios.get('/api/users')
+    },
+    registerUser: (user_name, password) => {
+        return axios.post('/auth/register', { username: user_name, password: password })
+    },
+    loginUser: (username, password) => {
+        return axios.post("/auth/login", { username: username, password: password })
+    },
+    Equipment: (bikeFrame, bikeType) => {
+        return axios.post("/api/bike"), { bikeFrame: bikeFrame, bikeType: bikeType}}
+};
--- src/Pages/User/index.js
+++ src/Pages/User/index.js
@@ -1,45 +1,59 @@
-import React, { useState } from "react";
+import React, { useState, Component } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-import "./User.css";
+import { API } from "../../Utils/userAPI";
+import "./";
+
+const user = (props) => {
+
+    const [bikeFrame, setFrame] = useState('')
+    const [bikeType, setType] = useState('')
+
+    const handleSubmit = (e) => {
+        API.Equipment(bikeFrame, bikeType,).then(res => {
+            alert(JSON.stringify(res))
+        })
+        e.preventDefault();
+    }
 
 
 
-<script> 
-    <body/>
-<h1>
-  Welcome to BikeAble
-  <small class="text-muted">insert username</small>
-</h1>
+
+    return (
+        <div className="Equipment">
+            
+            <Form onSubmit={handleSubmit}>
+                <Form.Group size="lg" className="bikeFrame" controlId="bikeFrame">
+                    <Form.Label>Bike Frame</Form.Label>
+                    <Form.Control
+                        
+                        type="bikeFrame"
+                        value={bikeFrame}
+                        onChange={(e) => setFrame(e.target.value)}
+                    />
+                </Form.Group>
+                <Form.Group size="lg" className="bikeType" controlId="bikeType">
+                    <Form.Label>Bike Type</Form.Label>
+                    <Form.Control
+                        type="bikeType"
+                        value={bikeType}
+                        onChange={(e) => setType(e.target.value)}
+                    />
+                </Form.Group>
+                <Button block size="lg" className="submitButton" type="submit">
+                    Submit
+        </Button>
+                
+        
+            </Form>
+        </div>
+    );
+}
 
 
-<h2>Store cycling equipment</h2>
-<div class="row"/>
-  
-  <div class="col"/>
-    <input type="text" class="form-control" placeholder="Bike Manufacturer" aria-label="Frame Name"/>
-  <div/>
-<div/>
+export default Equipment;
 
-<div class="form-check"/>
-  <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked/>
-  <label class="form-check-label" for="exampleRadios1">
-    Road Bike
-  </label>
-<div/>
-<div class="form-check"/>
-  <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2"/>
-  <label class="form-check-label" for="exampleRadios2">
-    Gravel Bike
-  </label>
-<div/>
-<div class="form-check"/>
-  <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios3" value="option3"/>
-  <label class="form-check-label" for="exampleRadios3">
-    Mountain Bike
-  </label>
-<div/>
-<button type="button" class="btn btn-primary">Enter Setup</button>
 
-<body/>
-</script>
+
+
+
