dd48e7ccdbbfec33ff8893267a4bf4ff14863009
Rachel Rosenstein <rachelrosenstein1@gmail.com>
2021-02-06 12:14:05 -0500

    registration working and rerouted to login page

--- client/src/Pages/Login/Registration.js
+++ client/src/Pages/Login/Registration.js
@@ -1,50 +1,78 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
-import './Login.css';
+import React, { useState, Component } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import { API } from "../../Utils/userAPI";
+import "./Login.css";
+import { useHistory, } from "react-router-dom";
 
-class Create extends Component {
+const Register = (props) => {
+    let history = useHistory();
+    const [firstName, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [zipCode, setZipCode] = useState('')
 
-    constructor() {
-        super();
-        this.state = {
-            username: '',
-            password: ''
-        };
-    }
-    onChange = (e) => {
-        const state = this.state
-        state[e.target.name] = e.target.value;
-        this.setState(state);
-    }
-
-    onSubmit = (e) => {
+    const handleSubmit = (e) => {
         e.preventDefault();
-
-        const { username, password } = this.state;
-
-        axios.post('/api/auth/register', { username, password })
-            .then((result) => {
-                this.props.history.push("/login")
-            });
+        API.registerUser(firstName, email, password, zipCode).then(res => {
+            // This is whre I redirect
+            if (res.data.success) {
+                history.push('/')
+            }
+        })
     }
 
-    render() {
-        const { username, password } = this.state;
-        return (
-            <div class="container">
-                <form class="form-signin" onSubmit={this.onSubmit}>
-                    <h2 class="form-signin-heading">Register</h2>
-                    <label for="inputEmail" class="sr-only">Email address</label>
-                    <input type="email" class="form-control" placeholder="Email address" name="username" value={username} onChange={this.onChange} required />
-                    <label for="inputPassword" class="sr-only">Password</label>
-                    <input type="password" class="form-control" placeholder="Password" name="password" value={password} onChange={this.onChange} required />
-                    <button class="btn btn-lg btn-primary btn-block" type="submit">Register</button>
-                </form>
-            </div>
-        );
-    }
+    // const redirectToReferrer = this.state.redirectToReferrer;
+    // if (redirectToReferrer === true) {
+    //     return <Redirect to="/dashboard" />
+    // }
+
+
+    return (
+        <div className="Login">
+            <h1>Register for Bikeable!</h1>
+            <Form onSubmit={handleSubmit}>
+                <Form.Group size="lg" className="email" controlId="firstName" >
+                    <Form.Label>First Name</Form.Label>
+                    <Form.Control
+                        autoFocus
+                        type="firstName"
+                        value={firstName}
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                </Form.Group>
+                <Form.Group size="lg" className="email" controlId="email" >
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control
+                        autoFocus
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </Form.Group>
+                <Form.Group size="lg" className="password" controlId="password">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </Form.Group>
+                <Form.Group size="lg" className="password" controlId="zipCode">
+                    <Form.Label>Zip Code</Form.Label>
+                    <Form.Control
+                        type="zipCode"
+                        value={zipCode}
+                        onChange={(e) => setZipCode(e.target.value)}
+                    />
+                </Form.Group>
+                <Button block size="lg" className="sign" type="submit">
+                    Sign Up!
+        </Button>
+            </Form>
+        </div>
+    );
 }
 
-export default Create;
+
+export default Register;
