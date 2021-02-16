17d88d70c0be03288c7c0be3b8b1d8fc935af8ab
Rachel Rosenstein <rachelrosenstein1@gmail.com>
2021-02-15 19:00:34 -0500

    added users with zipCode

--- client/src/Pages/Discover/index.js
+++ client/src/Pages/Discover/index.js
@@ -10,37 +10,31 @@ import { UsersList, UserListItem } from "./nearList"
 function Users() {
 
     const [zipCode, setZipCodeSearch] = useState([]);
+    const [users, setUsers] = useState([])
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        API.getZipUsers(setZipCodeSearch).then(res => {
-            console.log(res)
+        API.getZipUsers(zipCode).then(res => {
+            console.log(res.data)
+            setUsers({ users: res.data })
+            setZipCodeSearch("")
 
         }).catch(error => console.log(error))
     }
-
 
     return (
         <Table>
             <h1>Users in Your Area!</h1>
             <Container>
                 <Form>
-                    <Form.Control type="zipCode" className="zipCode" placeholder="Enter your Zip Code" />
+                    <Form.Control onChange={(e) => setZipCodeSearch(e.target.value)} type="text" className="zipCode" placeholder="Enter your Zip Code" />
                     <Button
                         onClick={handleSubmit}
                         block size="lg" className="zipButton">
                         Find Cyclers
             </Button>
                 </Form>
-                {/* <UsersList>
-                    {this.state.Users.map(users => {
-                        return (
-                            <UserListItem
-                                name={users.firstName}
-                                zipCode={users.zipCode}
-                            />);
-                    })}
-                </UsersList> */}
+                {users.length > 0 ? users.map((user, index) => <p>{JSON.stringify(user)}</p>) : "No users found"}
             </Container>
 
         </Table>
--- client/src/Pages/User/index.js
+++ client/src/Pages/User/index.js
@@ -19,9 +19,7 @@ const User = (props) => {
 
     
 
-    
 
-        
 
 
     return (
